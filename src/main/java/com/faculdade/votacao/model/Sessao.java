d0bc98f7754ab8ed3a5f0fdeec45f1b722393009
sylviavitoria <sylviavitoriasilva@gmail.com>
2025-05-17 20:59:40 -0300

    feat: adicionar logica de agendamento

--- src/main/java/com/faculdade/votacao/model/Sessao.java
+++ src/main/java/com/faculdade/votacao/model/Sessao.java
@@ -15,34 +15,54 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "tb_sessoes") 
+@Table(name = "tb_sessoes")
 @Getter
 @Setter
 public class Sessao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @JsonBackReference
     @OneToOne
     @JoinColumn(name = "pauta_id")
     private Pauta pauta;
-    
+
     private LocalDateTime dataAbertura;
-    
+
     private LocalDateTime dataFechamento;
-    
+
     public Sessao() {
-        
+
     }
-    
+
+    @Override
+    public String toString() {
+        return "Sessao [id=" + id + ", pautaId=" + (pauta != null ? pauta.getId() : null) + "]";
+    }
+
     public boolean isAberta() {
         LocalDateTime agora = LocalDateTime.now();
         return agora.isAfter(dataAbertura) && agora.isBefore(dataFechamento);
     }
-    
-    @Override
-    public String toString() {
-        return "Sessao [id=" + id + ", pautaId=" + (pauta != null ? pauta.getId() : null) + "]";
+
+    public boolean isAgendada() {
+        LocalDateTime agora = LocalDateTime.now();
+        return agora.isBefore(dataAbertura);
+    }
+
+    public boolean isEncerrada() {
+        LocalDateTime agora = LocalDateTime.now();
+        return agora.isAfter(dataFechamento);
+    }
+
+    public String getStatus() {
+        if (isAgendada()) {
+            return "AGENDADA";
+        } else if (isAberta()) {
+            return "ABERTA";
+        } else {
+            return "ENCERRADA";
+        }
     }
 }
