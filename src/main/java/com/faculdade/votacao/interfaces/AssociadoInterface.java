4bcec04e2c5ffb20553ac27564bc15cb6d13cbf2
sylviavitoria <sylviavitoriasilva@gmail.com>
2025-05-17 20:59:03 -0300

    refactor: Atualizar interfaces para usar DTOs nas operações

--- src/main/java/com/faculdade/votacao/interfaces/AssociadoInterface.java
+++ src/main/java/com/faculdade/votacao/interfaces/AssociadoInterface.java
@@ -3,10 +3,13 @@ package com.faculdade.votacao.interfaces;
 import java.util.List;
 import java.util.Optional;
 
+import com.faculdade.votacao.dto.AssociadoRequestDTO;
+import com.faculdade.votacao.dto.AssociadoResponseDTO;
 import com.faculdade.votacao.model.Associado;
 
 public interface AssociadoInterface {
-    Associado cadastrar(Associado associado);
+    AssociadoResponseDTO cadastrar(AssociadoRequestDTO associado);
+    AssociadoResponseDTO buscarAssociado(Long id);
     Optional<Associado> buscarPorId(Long id);
-    List<Associado> listarTodos();
+    List<AssociadoResponseDTO> listarTodos();
 }
