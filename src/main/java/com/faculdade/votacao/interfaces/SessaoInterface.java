0ecd13be0c519b2815337e858f20f5ecfb220992
HenriquePessoaLopes <henriquepessoa.lopes000@gmail.com>
2025-05-19 13:01:39 -0300

    feat: atualizar interfaces para utilizar DTOs nas operações de cadastro e busca

--- src/main/java/com/faculdade/votacao/interfaces/SessaoInterface.java
+++ src/main/java/com/faculdade/votacao/interfaces/SessaoInterface.java
@@ -1,10 +1,14 @@
 package com.faculdade.votacao.interfaces;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
+import com.faculdade.votacao.dto.SessaoResponseDTO;
 import com.faculdade.votacao.model.Sessao;
 
 public interface SessaoInterface {
-    Sessao abrirSessao(Long pautaId, Integer minutos);
+    SessaoResponseDTO abrirSessao(Long pautaId, Integer minutos);
+    SessaoResponseDTO agendarSessao(Long pautaId, LocalDateTime dataInicio, LocalDateTime dataFim, Integer duracaoMinutos);
+    SessaoResponseDTO buscarSessaoPorId(Long id);
     Optional<Sessao> buscarPorId(Long id);
 }
